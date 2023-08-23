fde6d5e9d2857741b9d648342e2cc84554fd49bf
calistair <calistair@outlook.com>
2023-08-23 14:47:23 +0100

    Made email form work and messages can be sent to my email now

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+function contact() {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+        .sendForm(
+            "service_vnub4b9",
+            "template_c9m66qm",
+            event.target,
+            "ayk-pGC6LuVij3L8V"
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The Email service is temporarily unavailable. Please contact me directly on calistair@outlook.com"
+            );
+        })
+}
+
