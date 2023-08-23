a3050df0b3b1d2bb49909aebaeb5ac50bab78584
calistair <calistair@outlook.com>
2023-08-23 15:48:12 +0100

    made dark theme

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+let isModalOpen = false;
+let contrastToggle = false;
 
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact() {
     event.preventDefault();
@@ -22,13 +33,12 @@ function contact() {
             );
         })
 }
-let isModalOpen =false;
-function toggleModal(){
+function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false
         return document.body.classList.remove("modal--open")
     }
     isModalOpen = true;
-    document.body.classList +=" modal--open"
+    document.body.classList += " modal--open"
 }
 
